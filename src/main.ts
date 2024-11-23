1553cd8bfb24fde868566cd8ff3f9d2957332ee3
Paulina0209 <paulina.medina@eia.edu.co>
2024-11-23 16:37:50 -0500

    corrección dot env

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+// Configurar dotenv para cargar variables de entorno desde .env
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS para solicitudes desde otros dominios (como el frontend)
   app.enableCors();
+
+  // Usar puerto dinámico para despliegue o 3000 como valor por defecto en local
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
 
